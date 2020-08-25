ea340899c6c36ba5d461811ee0a94f5426f70eab
Roy Amar <royammar030783@gmail.com>
2020-08-25 17:45:19 +0300

    second

--- src/cmps/NavBar.jsx
+++ src/cmps/NavBar.jsx
@@ -4,7 +4,7 @@ import { NavLink} from 'react-router-dom'
 export default function navBar() {
     return (
         <div className="nav-bar container  flex space-between">
-            <NavLink activeClassName="selected"  className="home" to="/" exact>Home</NavLink>
+            <NavLink activeClassName="selected"  className="home" to="/weightTracker" exact>Home</NavLink>
             <NavLink activeClassName="selected"  className="history" to="/history" exact>History</NavLink> 
             <NavLink activeClassName="selected"  className="history" to="/months" exact> Months</NavLink> 
             
