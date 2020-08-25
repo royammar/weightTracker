0be0510a43ed14347d27af87186695dc83aed226
Roy Amar <royammar030783@gmail.com>
2020-08-25 17:46:58 +0300

    Updates

---
+++ precache-manifest.64e3f523014dc6af35c7679788781725.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33820b552053ffa5a8909bd45361ef0",
+    "url": "/weightTracker/index.html"
+  },
+  {
+    "revision": "5a0d37b0d0a3bdf5257a",
+    "url": "/weightTracker/static/css/main.32d8de96.chunk.css"
+  },
+  {
+    "revision": "964e32c6399380dd9d5f",
+    "url": "/weightTracker/static/js/2.65423dad.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/weightTracker/static/js/2.65423dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a0d37b0d0a3bdf5257a",
+    "url": "/weightTracker/static/js/main.86697300.chunk.js"
+  },
+  {
+    "revision": "cec443836cdf48ccffcd",
+    "url": "/weightTracker/static/js/runtime-main.4bf54cfe.js"
+  },
+  {
+    "revision": "9406d0ab606cf8cb91c41b65556bd836",
+    "url": "/weightTracker/static/media/Lobster-Regular.9406d0ab.ttf"
+  },
+  {
+    "revision": "a6e5446a7c5789aabc9b37eaaf72134d",
+    "url": "/weightTracker/static/media/Oswald-VariableFont_wght.a6e5446a.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/weightTracker/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
